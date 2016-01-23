62fd7f8607e9d424e5ad3e37b9f383b8316e461f
Maria Geller <maria.geller2@gmail.com>
2016-01-23 22:12:28 +1300

    Remove cljs-compat.

--- plugin/project.clj
+++ plugin/project.clj
@@ -1,13 +1,11 @@
-(defproject lein-cljsbuild "1.1.2"
+(defproject lein-cljsbuild "2.0.0-SNAPSHOT"
   :description "ClojureScript Autobuilder Plugin"
   :url "http://github.com/emezeske/lein-cljsbuild"
   :license
     {:name "Eclipse Public License - v 1.0"
      :url "http://www.eclipse.org/legal/epl-v10.html"
      :distribution :repo}
-    :dependencies [; SNAPSHOT is intentional here, https://github.com/emezeske/lein-cljsbuild/issues/266
-                   [lein-cljsbuild/cljs-compat "1.0.0-SNAPSHOT"]
-                   [fs "1.1.2"]]
+    :dependencies [[fs "1.1.2"]]
   :profiles {
     :dev {
       :dependencies [
